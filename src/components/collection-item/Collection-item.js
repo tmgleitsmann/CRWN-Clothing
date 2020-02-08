ae7c4449ad9d2a7c9a1f4f6acfe0e37cc2e16d41
Tom Gleitsmann <gleitsmann@Toms-MacBook-Air.local>
2020-02-08 13:40:44 -0600

    updated shop data with new data. updated cart utilities to group items in cart reducer

--- src/components/collection-item/Collection-item.js
+++ src/components/collection-item/Collection-item.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import CustomButtom from '../button/Custom-button';
+import { addItem } from '../../redux/cart/cart-actions';
 
 import '../../styles/components/Collection-item.styles.scss';
 
-const CollectionItem = ({id, name, price, imageUrl}) => (
-    <div className = 'collection-item'>
-        <div className = 'image' style={{backgroundImage: `url(${imageUrl})`}} />
-        <div className='collection-footer'>
-            <span className='name'>{name}</span>
-            <span className='price'>{price}</span>
+const CollectionItem = ({ item, addItem }) => {
+    const { name, price, imageUrl } = item;
+    return(
+        <div className = 'collection-item'>
+            <div className = 'image' style={{backgroundImage: `url(${imageUrl})`}} />
+            <div className='collection-footer'>
+                <span className='name'>{name}</span>
+                <span className='price'>{price}</span>
+            </div>
+            <CustomButtom onClick={() => addItem(item)} inverted> Add To Cart </CustomButtom>
         </div>
-    </div>
-);
+    )
+};
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import {Link} from 'react-router-dom';
+import CartIcon from '../cart-icon/Cart-icon';
+import CartDropdown from '../cart-dropdown/Cart-dropdown';
 import { auth } from '../../firebase/Firebase.utils';
 import { ReactComponent as Logo } from '../../images/crown.svg';
 import '../../styles/components/Header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className = 'header'>
         <Link to = '/' className='logo-container'>
             <Logo className='logo'/>
@@ -23,12 +25,15 @@ const Header = ({ currentUser }) => (
                     : 
                 <Link className = 'option' to='/sign-in'>SIGN IN</Link>
             }
+            <CartIcon />
         </div>
+        { hidden ? null: (<CartDropdown />)}
     </div>
 );
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+    currentUser,
+    hidden
 });
 
 export default connect(mapStateToProps)(Header);
